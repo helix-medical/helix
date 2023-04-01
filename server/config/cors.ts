7e60b48933951ed02290d514b246c57e7384ead8
Xavier2p <xavier.de-place@epita.fr>
2023-04-01 20:38:54 +0200

    cors config done, but not implemented due to insomnia

---
+++ server/config/cors.ts
@@ -0,0 +1,14 @@
+import cors, { CorsOptions } from 'cors';
+const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin ?? '') !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200,
+};
+
+export default () => cors(corsOptions);
