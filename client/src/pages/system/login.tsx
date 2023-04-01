57deb26796697694ee95cd36b19fc934d878e51f
Xavier2p <xavier.de-place@epita.fr>
2023-04-01 16:53:09 +0200

    create a minimal login page

--- client/src/pages/system/login.tsx
+++ client/src/pages/system/login.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
-import { Title } from '@mantine/core';
+import React, { useState } from 'react';
+import { isNotEmpty, useForm } from '@mantine/form';
+import { TextInput, PasswordInput, Anchor, Paper, Title, Container, Group, Button } from '@mantine/core';
+import { IconLock, IconUserSearch } from '@tabler/icons-react';
 
-const Login = (): JSX.Element => {
+const Login = () => {
+    const form = useForm({
+        initialValues: {
+            account: '',
+            password: '',
+        },
+        validate: {
+            account: isNotEmpty('Account is required'),
+            password: isNotEmpty('Password is required'),
+        },
+    });
+
+    const handleSubmit = () => {
+        console.log(form.values);
+        setLoading(true);
+    };
+
+    const [loading, setLoading] = useState(false);
+
     return (
-        <div>
-            <Title order={1}>Login</Title>
-        </div>
+        <Container size={420} my={40}>
+            <Title align="center">Log in to Helix</Title>
+            {/* <Text color="dimmed" size="sm" align="center" mt={5}>
+                Do not have an account yet?{' '}
+                <Anchor size="sm" component="button">
+                    Create account
+                </Anchor>
+            </Text> */}
+
+            <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+                <form onSubmit={form.onSubmit(handleSubmit)}>
+                    <TextInput
+                        label="Account"
+                        placeholder="Maivy"
+                        withAsterisk
+                        {...form.getInputProps('account')}
+                        icon={<IconUserSearch size="1rem" />}
+                    />
+                    <PasswordInput
+                        label="Password"
+                        placeholder="Je t'aime"
+                        withAsterisk
+                        mt="md"
+                        icon={<IconLock size="1rem" />}
+                        {...form.getInputProps('password')}
+                    />
+                    <Group position="center" mt="lg">
+                        <Anchor component="a" size="sm" href="mailto:admin@helix.dev">
+                            Contact administrator
+                        </Anchor>
+                    </Group>
+                    <Button fullWidth mt="xl" type="submit" loading={loading}>
+                        Sign in
+                    </Button>
+                </form>
+            </Paper>
+        </Container>
     );
 };
 
