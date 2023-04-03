21537710a74786118082c7501717152d9be015d1
Xavier2p <xavier.de-place@epita.fr>
2023-04-03 15:30:06 +0200

    implement jwt

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -3,11 +3,8 @@ import db from '../database/config';
 import logger from '../system/logger';
 import sc from '../tools/statusCodes';
 import bcrypt from 'bcrypt';
-
-interface IUser {
-    uid: string;
-    password: string;
-}
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
 
 const login = async (req: Request, res: Response) => {
     logger.post(req.originalUrl, 'REQ');
@@ -15,26 +12,45 @@ const login = async (req: Request, res: Response) => {
     const sqlQuery = `SELECT * FROM users WHERE uid = ?`;
     const values = [id];
 
-    db.query(sqlQuery, values, (err: any, data: any) => {
+    const result: any = db.query(sqlQuery, values, (err: any, data: any) => {
         if (err) {
             logger.post(req.originalUrl, 'ERR', err);
-            return res.status(sc.METHOD_FAILURE).json(err);
+            return { fail: true, status: sc.METHOD_FAILURE, message: err.message };
         }
 
         if (data.length === 0) {
             logger.post(req.originalUrl, 'ERR', 'User does not exist');
-            return res.status(sc.NOT_FOUND).json({ message: 'User does not exist' });
+            return { fail: true, status: sc.NOT_FOUND, message: 'User does not exist' };
         }
 
-        const user = data[0];
-        if (!bcrypt.compareSync(password, user.password)) {
-            logger.post(req.originalUrl, 'ERR', 'Invalid password');
-            return res.status(sc.UNAUTHORIZED).json({ message: 'Invalid password' });
-        }
-
-        logger.post(req.originalUrl, 'OK', `User ${id} logged in`);
-        return res.status(sc.ACCEPTED).json({ id: id, message: `User successfully logged in` });
+        return { fail: false, user: data[0] };
     });
+
+    if (result.fail) {
+        return res.status(result.status).json({ message: result.message });
+    }
+
+    const user = result.user;
+
+    if (!bcrypt.compareSync(password, user.password)) {
+        logger.post(req.originalUrl, 'ERR', 'Invalid password');
+        return res.status(sc.UNAUTHORIZED).json({ message: 'Invalid password' });
+    }
+
+    const accessToken = jwt.sign({ id: user.uid }, process.env.ACCESS_TOKEN_SECRET as string, { expiresIn: '30s' });
+    const refreshToken = jwt.sign({ id: user.uid }, process.env.REFRESH_TOKEN_SECRET as string, { expiresIn: '12h' });
+    const sqlQuery2 = 'UPDATE users SET refreshToken = ? WHERE uid = ?';
+    const values2 = [refreshToken, user.uid];
+    db.query(sqlQuery2, values2, (err: any, data: any) => {
+        if (err) {
+            logger.post(req.originalUrl, 'ERR', err);
+            return res.status(sc.METHOD_FAILURE).json({ message: err.message });
+        }
+        logger.success('Refresh token added to database');
+    });
+    logger.post(req.originalUrl, 'OK', `User ${id} logged in`);
+    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 12 * 60 * 60 * 1000 });
+    return res.status(sc.ACCEPTED).json({ id: id, message: `User successfully logged in`, token: accessToken });
 };
 
 export default module.exports = {
--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -3,8 +3,10 @@ import validate from '../validation/validator';
 import logger from '../system/logger';
 import sc from '../tools/statusCodes';
 import queries from '../database/queries';
+import jwt from 'jsonwebtoken';
+require('dotenv').config();
 
-const login = async (req: Request, res: Response, next: NextFunction) => {
+const login = (req: Request, res: Response, next: NextFunction) => {
     logger.use(req.originalUrl, 'REQ');
     const isValid = validate.login(req.body);
     if (!isValid) {
@@ -16,6 +18,25 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
     next();
 };
 
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    logger.use(req.originalUrl, 'REQ');
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.status(sc.UNAUTHORIZED).json({ message: 'No token provided' });
+    logger.debug(authHeader);
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: any, decoded: any) => {
+        if (err) {
+            logger.use(req.originalUrl, 'ERR', err);
+            return res.status(sc.FORBIDDEN).json({ message: 'Invalid token' });
+        }
+        logger.use(req.originalUrl, 'OK', 'Valid token');
+        req.body.id = decoded.id;
+    });
+
+    // next();
+};
+
 export default module.exports = {
     login,
+    verifyToken,
 };
