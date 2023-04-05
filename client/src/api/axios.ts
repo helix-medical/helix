271d618d0d9e534e6976ccd959d40636eaf22254
Xavier2p <xavier.de-place@epita.fr>
2023-04-05 10:55:41 +0200

    Split repos

--- client/src/api/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const axiosPrivate = axios.create({
-    baseURL: 'http://localhost:3001',
-    headers: { 'Content-Type': 'application/json' },
-    withCredentials: true,
-});
--- client/src/tools/getLines.ts
+++
@@ -1,6 +0,0 @@
-function getNbLines(input: string, limit?: number) {
-    const len = input.split(/\r\n|\r|\n/).length;
-    return limit && len > limit ? limit : len;
-}
-
-export default getNbLines;
