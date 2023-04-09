97b2ec2a24575782788da638953a1ac31947f1e3
Xavier2p <xavier.de-place@epita.fr>
2023-04-09 22:48:48 +0200

    Changes on `Calendar`

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -2,23 +2,40 @@ import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Badge, Title } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import cnf from '../config/config';
 
-const events = [
-    {
-        start: moment('2023-04-05 12:34').toDate(),
-        end: moment().toDate(),
-        title: 'Some title',
-    },
-];
+// const events = [
+// {
+// start: moment('2023-04-05 12:34').toDate(),
+// end: moment().toDate(),
+// title: 'Some title',
+// },
+// ];
 
 const Calendar = () => {
+    const [events, setEvents] = useState([]);
+    useEffect(() => {
+        const fetchEvents = async () => {
+            const res = await axios.get('api/appointments');
+            const events = res.data.map((event: any) => ({
+                start: moment(event.date).toDate(),
+                end: moment(event.date).add(cnf.durationAppointment, 'minute').toDate(),
+                title: `Appointment n° ${event.id}`,
+            }));
+            setEvents(events);
+        };
+        fetchEvents();
+    }, []);
+
     const localizer = momentLocalizer(moment);
     return (
         <>
             <Title order={1} mb="xl">
                 Calendar <Badge color="teal">Beta</Badge>
             </Title>
-            <BigCalendar localizer={localizer} events={events} toolbar={false} defaultView="week" />
+            <BigCalendar localizer={localizer} events={events} defaultView="week" />
         </>
     );
 };
