9789907f6911c9a891743162b5f308916385523e
Xavier2p <xavier.de-place@epita.fr>
2023-04-11 18:31:42 +0200

    Work on calendar

--- src/components/calendar.tsx
+++ src/components/calendar.tsx
@@ -1,18 +1,10 @@
-import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar';
+import { Calendar as BigCalendar, DateLocalizer, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Badge, Paper, Title } from '@mantine/core';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
 import cnf from '../config/config';
-
-// const events = [
-// {
-// start: moment('2023-04-05 12:34').toDate(),
-// end: moment().toDate(),
-// title: 'Some title',
-// },
-// ];
 
 const Calendar = () => {
     const [events, setEvents] = useState([]);
@@ -22,21 +14,74 @@ const Calendar = () => {
             const events = res.data.map((event: any) => ({
                 start: moment(event.date).toDate(),
                 end: moment(event.date).add(cnf.durationAppointment, 'minute').toDate(),
-                title: `Appointment n° ${event.id}`,
+                title: `${event.name} ${event.lastName}`,
             }));
             setEvents(events);
         };
         fetchEvents();
     }, []);
 
-    const localizer = momentLocalizer(moment);
+    const slotGroupPropGetter = useCallback(
+        () => ({
+            style: {
+                minHeight: 60,
+            },
+        }),
+        []
+    );
+
+    const { formats, localizer } = useMemo(
+        () => ({
+            formats: {
+                selectRangeFormat: (
+                    { start, end }: { start: Date; end: Date },
+                    culture: string,
+                    localizer: DateLocalizer
+                ) => localizer.format(start, 'HH:mm', culture) + '+' + localizer.format(end, 'hh:mm', culture),
+                dayFormat: (date: Date, culture: string, localizer: DateLocalizer) =>
+                    localizer.format(date, 'ddd MM/DD', culture),
+            },
+            localizer: momentLocalizer(moment),
+            // messages: {
+            //     date: 'Date',
+            //     time: 'Time',
+            //     event: 'Event',
+            //     allDay: 'All Day',
+            //     week: 'Week',
+            //     work_week: 'Work Week',
+            //     day: 'Day',
+            //     month: 'Month',
+            //     previous: 'Back',
+            //     next: 'Next',
+            //     yesterday: 'Yesterday',
+            //     tomorrow: 'Tomorrow',
+            //     today: 'Today',
+            //     agenda: 'Agenda',
+            //     noEventsInRange: 'There are no events in this range.',
+            //     showMore: (total: number) => `+${total} more`,
+            // },
+        }),
+        []
+    );
     return (
         <>
             <Title order={1} mb="xl">
-                Calendar <Badge color="teal">Beta</Badge>
+                Calendar{' '}
+                <Badge color="teal" size="xl">
+                    Beta
+                </Badge>
             </Title>
             <Paper shadow="sm" radius="md" p="lg" withBorder my="lg">
-                <BigCalendar localizer={localizer} events={events} defaultView="week" />
+                <BigCalendar
+                    localizer={localizer}
+                    events={events}
+                    defaultView="week"
+                    formats={formats as any}
+                    max={new Date(1972, 0, 1, 19, 59, 59)}
+                    min={new Date(1972, 0, 1, 8, 0, 0)}
+                    slotGroupPropGetter={slotGroupPropGetter}
+                    views={['week', 'day', 'agenda']}
+                />
             </Paper>
         </>
     );
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,5 @@
 export interface IPatient {
-    id?: string;
+    id: string;
     name: string;
     lastName: string;
     birthDate: string;
