478c7f0b42d93f73e08d8378c724243ef9a52a0c
Xavier2p <xavier.de-place@epita.fr>
2023-04-13 16:29:03 +0200

    Responsive `Accounting`

--- src/pages/accounting/quickView.tsx
+++ src/pages/accounting/quickView.tsx
@@ -57,14 +57,14 @@ const QuickView = () => {
     return (
         <Paper shadow="sm" radius="md" p="lg" withBorder my="lg">
             <Title order={2}>Quick View</Title>
-            <Grid columns={3} align="center" p="md">
-                <Grid.Col span={1}>
+            <Grid columns={12} align="center" p="md">
+                <Grid.Col xs={12} sm={6} md={6} lg={4} xl={4}>
                     <ItemQuickView sum={sumMonth as ISum} name="This Month" />
                 </Grid.Col>
-                <Grid.Col span={1}>
+                <Grid.Col xs={12} sm={6} md={6} lg={4} xl={4}>
                     <ItemQuickView sum={sumWeek as ISum} name="This Week" />
                 </Grid.Col>
-                <Grid.Col span={1}>
+                <Grid.Col xs={12} sm={12} md={12} lg={4} xl={4}>
                     <ItemQuickView sum={sumAll as ISum} name="All Time" />
                 </Grid.Col>
             </Grid>
