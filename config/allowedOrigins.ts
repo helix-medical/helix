49e5cce4530addf69d3cf5f41a03a413d15a1ca4
Xavier2p <xavier.de-place@epita.fr>
2023-04-19 12:29:33 +0200

    Helix is deployable

--- config/allowedOrigins.ts
+++ config/allowedOrigins.ts
@@ -1,3 +1,11 @@
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:3001'];
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://10.11.0.4',
+    'http://10.11.0.1',
+    'http://10.10.0.1',
+    'http://10.10.0.4',
+    'http://localhost:8080',
+];
 
 export default allowedOrigins;
