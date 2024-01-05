d9d0cdfd6b7dfbf1ea7c9994ce829a5459af1522
Xavier2p <xavier.de-place@epita.fr>
2024-01-05 17:53:12 +0100

    fix: errors in image build

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
     plugins: [react()],
-    base: '/helix'
-});
+    base: mode == 'production' ? '/helix' : '/',
+  });
+};
